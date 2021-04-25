a03e29eca4e8b94e0bc19866b4fa327f8d99f561
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-04-25 16:50:23 -0700

    test API call seems to work.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,2 +1,12 @@
+// Open Weather API Key 5d70557326e750e6419dd74315c00fd5
+let APIkey = '5d70557326e750e6419dd74315c00fd5'
+let city = 'Phoenix';
+let queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`;
 
+$.ajax({
+    url: queryURL,
+    method: 'get'
+}).then((res) => {
+    console.log(res);
+})
     
