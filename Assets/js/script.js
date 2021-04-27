b26c92e7909d8055c14eff919b6c5a48c38048ba
EliptickBlock5 <nbjohnson@gmail.com>
2021-04-27 12:01:20 -0700

    initial calls in jquery

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,26 @@
 // Open Weather API Key 5d70557326e750e6419dd74315c00fd5
 let APIkey = '5d70557326e750e6419dd74315c00fd5'
-let city = 'Phoenix';
-let queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`;
+$('.input-group-text').click(getCityWeather);
 
-$.ajax({
-    url: queryURL,
-    method: 'get'
-}).then((res) => {
-    console.log(res);
-})
-    
+function getCityWeather() {
+    let city = $('.input-search').val();
+    let queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIkey}`;
+
+    console.log(city);
+
+    $.ajax({
+        url: queryURL,
+        method: 'get'
+    }).then((res) => {
+        console.log(res);
+        $('.city-heading').text(res.name);
+        $('.temp').text(res.main.temp);
+        $('.humidity').text(res.main.humidity);
+        $('.wind-speed').text(res.wind.speed);
+        //$('.uv-index').text(res.main.temp);
+        $('.input-search').val('')
+    })
+}
+
+
+
