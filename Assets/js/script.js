440894ccd7fdd81a2214de2dbe9f6bee12cf8fcd
EliptickBlock5 <nbjohnson@gmail.com>
2021-06-10 14:01:50 -0700

    api call working, weather displaying, formatting off

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,6 @@
   
 $(document).ready(function () {
-    var pastSearches = JSON.parse(localStorage.getItem("searches")) || [];
+    var pastSearches = JSON.parse(localStorage.getItem("pastSearches")) || [];
 
 
     function renderPastSearches() {
@@ -9,7 +9,7 @@ $(document).ready(function () {
         // trims history to five past searches
 
 
-        var lastFive = searches.slice(0, 4);
+        var lastFive = pastSearches.slice(0, 4);
         for (var i = 0; i < lastFive.length; i++) {
             $("#past-searches").append($("<a href='' class='city list-group-item'>").text(lastFive[i]));
         };
@@ -28,10 +28,10 @@ $(document).ready(function () {
         method: "GET"
     }).then(function (response) {
         // this should add city to search history
-        if (!searches.includes(response.name)) {
-            searches.push(response.name);
+        if (!pastSearches.includes(response.name)) {
+            pastSearches.push(response.name);
         }
-        localStorage.setItem("searches", JSON.stringify(searches));
+        localStorage.setItem("searches", JSON.stringify(pastSearches));
         $("#search").val("");
         renderPastSearches();
 
